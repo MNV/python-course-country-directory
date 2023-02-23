b7d968c4bb842fdf5f7040a307e889f71b37ae70
Aleksandr <klassen.aleksandr@yandex.ru>
2023-02-23 13:44:12 +0500

    Add new parameters in models and news model Make output in table Adding tests

--- src/clients/country.py
+++ src/clients/country.py
@@ -1,13 +1,7 @@
 """
 Функции для взаимодействия с внешним сервисом-провайдером данных о странах.
 """
-from http import HTTPStatus
-from typing import Optional
-
-import aiohttp
-
 from clients.base import BaseClient
-from logger import trace_config
 from settings import API_KEY_APILAYER
 
 
@@ -16,27 +10,16 @@ class CountryClient(BaseClient):
     Реализация функций для взаимодействия с внешним сервисом-провайдером данных о странах.
     """
 
+    headers = {"apikey": API_KEY_APILAYER}
+
     async def get_base_url(self) -> str:
         return "https://api.apilayer.com/geo/country"
 
-    async def _request(self, endpoint: str) -> Optional[dict]:
-
-        # формирование заголовков запроса
-        headers = {"apikey": API_KEY_APILAYER}
-
-        async with aiohttp.ClientSession(trace_configs=[trace_config]) as session:
-            async with session.get(endpoint, headers=headers) as response:
-                if response.status == HTTPStatus.OK:
-                    return await response.json()
-
-                return None
-
-    async def get_countries(self, bloc: str = "eu") -> Optional[dict]:
+    async def get_countries(self, bloc: str = "eu") -> dict | None:
         """
         Получение данных о странах.
 
         :param bloc: Регион
         :return:
         """
-
         return await self._request(f"{await self.get_base_url()}/regional_bloc/{bloc}")
